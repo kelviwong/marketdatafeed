3ab74d900034d7ae38be868336fb2ab0840fd718
kelvin <kelvinwongyw@gmail.com>
2025-03-15 23:56:28 +1100

    Retry

--- src/binance.rs
+++ src/binance.rs
@@ -46,50 +46,62 @@ impl ExchangeFeed for Binance {
         println!("connecting... {:?}", connection_str);
 
         // Connect to Binance WebSocket
-        let (mut ws_stream, _) = connect_async(connection_str).await.expect("connected.");
+        // let (mut ws_stream, _) = connect_async(connection_str).await.expect("connected.");
+        let mut ws_stream = None;
+        match connect_async(connection_str).await {
+            Ok((mut stream, _)) => {
+                // Connection was successful
+                ws_stream = Some(stream);
+                println!("Connected to WebSocket server");
+            }
+            Err(e) => {
+                // Handle the error if the connection fails
+                return Err(format!("Error Connecting: {:?}", e));
+            }
+        }
 
         on_success("Binance connected.".to_string());
+
+        let mut ws_stream = ws_stream.unwrap(); 
 
         let mut candle_stick_data = CandleStickBuilder::new().build();
         candle_stick_data.source = "Binance".to_string();
 
-        while let Some(Ok(msg)) = ws_stream.next().await {
-            match msg {
-                Message::Text(text) => {
-                    //     // Deserialize the received JSON message into a WebSocketMessage
-                    //     let _ws_message: WebSocketMessage =
-                    //         serde_json::from_str(&text).expect("Failed to deserialize message");
-                    //         candle_stick_data.ts = _ws_message.kline.open_time;
-                    //         candle_stick_data.c = _ws_message.kline.close.parse::<f64>().unwrap_or(0.0);
-                    //         candle_stick_data.o = _ws_message.kline.open.parse::<f64>().unwrap_or(0.0);
-                    //         candle_stick_data.h = _ws_message.kline.high.parse::<f64>().unwrap_or(0.0);
-                    //         candle_stick_data.l = _ws_message.kline.low.parse::<f64>().unwrap_or(0.0);
-                    //         candle_stick_data.v = _ws_message.kline.volume.parse::<f64>().unwrap_or(0.0);
-                    //      println!("CandleStick data: {:?}", candle_stick_data);
-                    match serde_json::from_str::<WebSocketMessage>(&text) {
-                        Ok(ws_message) => {
-                            // Update candle stick data if deserialization is successful
-                            candle_stick_data.ts = ws_message.kline.open_time;
-                            candle_stick_data.c =
-                                ws_message.kline.close.parse::<f64>().unwrap_or(0.0);
-                            candle_stick_data.o =
-                                ws_message.kline.open.parse::<f64>().unwrap_or(0.0);
-                            candle_stick_data.h =
-                                ws_message.kline.high.parse::<f64>().unwrap_or(0.0);
-                            candle_stick_data.l =
-                                ws_message.kline.low.parse::<f64>().unwrap_or(0.0);
-                            candle_stick_data.v =
-                                ws_message.kline.volume.parse::<f64>().unwrap_or(0.0);
+        while let Some(result) = ws_stream.next().await {
+            match result {
+                Ok(msg) => {
+                    match msg {
+                        Message::Text(text) => {
+                            match serde_json::from_str::<WebSocketMessage>(&text) {
+                                Ok(ws_message) => {
+                                    // Update candle stick data if deserialization is successful
+                                    candle_stick_data.ts = ws_message.kline.open_time;
+                                    candle_stick_data.c =
+                                        ws_message.kline.close.parse::<f64>().unwrap_or(0.0);
+                                    candle_stick_data.o =
+                                        ws_message.kline.open.parse::<f64>().unwrap_or(0.0);
+                                    candle_stick_data.h =
+                                        ws_message.kline.high.parse::<f64>().unwrap_or(0.0);
+                                    candle_stick_data.l =
+                                        ws_message.kline.low.parse::<f64>().unwrap_or(0.0);
+                                    candle_stick_data.v =
+                                        ws_message.kline.volume.parse::<f64>().unwrap_or(0.0);
 
-                            println!("CandleStick data: {:?}", candle_stick_data);
+                                    println!("CandleStick data: {:?}", candle_stick_data);
+                                }
+                                Err(e) => {
+                                    println!("Failed to deserialize message: {:?}", e);
+                                    println!("{:?}", text);
+                                }
+                            }
                         }
-                        Err(e) => {
-                            println!("Failed to deserialize message: {:?}", e);
-                            println!("{:?}", text);
-                        }
+                        _ => (),
                     }
                 }
-                _ => (),
+                Err(e) => {
+                    // Return error message in case of connection failure
+                    return Err(format!("Error receiving message: {:?}", e));
+                }
             }
         }
 
